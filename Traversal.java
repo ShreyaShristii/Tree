9eeaf13e97aaabde0e2b9366379a67d353b3eb14
Shreya Shristi <shristishreyac@gmail.com>
2025-12-15 23:37:32 +0530

    Initial commit

---
+++ Traversal.java
@@ -0,0 +1,85 @@
+// public class Traversal{
+//     public static void main(String args[]){
+// int[] nodes = {1, 2, 3, -1, -1, 3, -1, -1, 564, -1, 78, -1, -1};
+
+// Binarytree.Btree tree=new Binarytree.Btree();
+// Binarytree.Node root=tree.buildTree(nodes);
+//     }
+// }
+//  class Binarytree{
+//     static class Node{
+//         int data;
+//         Node left;
+//         Node right;
+//         Node(int data){
+//             this.data=data;
+//             this.left=null;
+//             this.right=null;
+
+//         }
+//     }
+//     static class Btree{
+//         static int idx=-1;
+//         public static Node buildTree(int nodes[]){
+//             idx++;
+//             // if(nodes[idx]==-1){
+//             //     return null;
+//             // }
+//             if (idx >= nodes.length || nodes[idx] == -1) {
+//             return null;
+//         }
+
+//             Node newNode=new Node(nodes[idx]);
+//             newNode.left=buildTree(nodes);
+//             newNode.right=buildTree(nodes);
+//             return newNode;
+//         }
+//     }
+// }
+public class Traversal {
+    public static void main(String args[]) {
+
+        int[] nodes = {1, 2, 3, -1, -1, 3, -1, -1, 564, -1, 78, -1, -1};
+
+        Binarytree.Btree tree = new Binarytree.Btree();
+        Binarytree.Node root = tree.buildTree(nodes);
+
+        preorder(root);
+    }
+
+    static void preorder(Binarytree.Node root) {
+        if (root == null) return;
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+}
+
+class Binarytree {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class Btree {
+        static int idx = -1;
+
+        static Node buildTree(int nodes[]) {
+            idx++;
+            if (idx >= nodes.length || nodes[idx] == -1) return null;
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+    }
+}
